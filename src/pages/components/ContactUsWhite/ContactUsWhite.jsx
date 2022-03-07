73401d003e3195f07142a99cd7a4c43557e145dc
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2022-03-07 08:50:53 -0500

    general fixes

--- src/pages/components/ContactUsWhite/ContactUsWhite.jsx
+++ src/pages/components/ContactUsWhite/ContactUsWhite.jsx
@@ -1,22 +1,43 @@
 import React, { useEffect, useState } from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import "./ContactUsWhite.scss";
 
+import "./ContactUsWhite.scss";
 
 const ContactUsWhite = () => {
   return (
     <>
       <div className="ContactUsWhite-cotainer container px-0 my-5">
         <div className="form-container-item col-12 col-lg-10 m-auto">
-          <h3 className="text-center">Get in touch and we'll get back to you as soon as we can. We look forward to hearing from you!</h3>
+          <h3 className="text-center">
+            Get in touch and we'll get back to you as soon as we can. We look
+            forward to hearing from you!
+          </h3>
           <form action="" className="form-contatcUs col-12 col-lg-10">
             <div className="user-data d-md-flex">
-              <input type="text" name="formName" id="formName" placeholder="Name" className="me-3 mb-3"/>
-              <input type="email" name="formEmail" id="formEmail" placeholder="Email" className=" mb-3"/>
+              <input
+                type="text"
+                name="formName"
+                id="formName"
+                placeholder="Name"
+                className="me-3 mb-3"
+              />
+              <input
+                type="email"
+                name="formEmail"
+                id="formEmail"
+                placeholder="Email"
+                className=" mb-3"
+              />
             </div>
-            <textarea name="formMessage" id="formMessage" className="formMessage" rows="10" placeholder="Message"></textarea>
-            <button type="submit" className="d-flex align-self-end mt-3">SUBMIT</button>
+            <textarea
+              name="formMessage"
+              id="formMessage"
+              className="formMessage"
+              rows="10"
+              placeholder="Message"
+            ></textarea>
+            <button type="submit" className="d-flex align-self-end mt-3">
+              SUBMIT
+            </button>
           </form>
         </div>
       </div>
--- src/pages/components/FeaturedPosts/FeaturedPosts.jsx
+++ src/pages/components/FeaturedPosts/FeaturedPosts.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import "./FeaturedPosts.scss";
 import { Link } from "gatsby";
+import { Container } from "react-bootstrap";
 
 const FeaturedPostQuery = "posts&&per_page=2&categories=8";
 const regex = /(<([^>]+)>)/gi;
@@ -36,35 +37,42 @@ const FeaturedPosts = () => {
     return (
       <Row className="featured-post-cotainer">
         <div className="row px-5 d-flex justify-content-center pre-container">
-          {featuredPosts.map((post, idx) => (
-            <div
-              key={idx}
-              className={
-                " col-md-4 card-container mx-4 p-5" + (idx === 1 ? " last" : "")
-              }
-            >
-              <h3 className="mb-3">{parseHtmlEnteties(post.title.rendered)}</h3>
-
-              <p className="description">
-                {post.excerpt.rendered.replace(regex, "")} ...
-              </p>
-              <p className="read-more">
-                <Link
-                  id={post.id}
-                  to={`/blog/${
-                    post.link.split(process.env.GATSBY_CMS_BASE_URL)[1]
-                  }`}
-                  state={{
-                    id: post.id,
-                    title: parseHtmlEnteties(post.title.rendered),
-                  }}
+          <Container style={{ maxWidth: "1320px" }}>
+            <div className="row d-flex justify-content-around">
+              {featuredPosts.map((post, idx) => (
+                <div
+                  key={idx}
+                  className={
+                    " col-md-4 card-container mx-4 p-5" +
+                    (idx === 1 ? " last" : "")
+                  }
                 >
-                  READ MORE →
-                </Link>
-              </p>
-              <div className="icon"></div>
+                  <h3 className="mb-3">
+                    {parseHtmlEnteties(post.title.rendered)}
+                  </h3>
+
+                  <p className="description">
+                    {post.excerpt.rendered.replace(regex, "")} ...
+                  </p>
+                  <p className="read-more">
+                    <Link
+                      id={post.id}
+                      to={`/blog/${
+                        post.link.split(process.env.GATSBY_CMS_BASE_URL)[1]
+                      }`}
+                      state={{
+                        id: post.id,
+                        title: parseHtmlEnteties(post.title.rendered),
+                      }}
+                    >
+                      READ MORE →
+                    </Link>
+                  </p>
+                  <div className="icon"></div>
+                </div>
+              ))}
             </div>
-          ))}
+          </Container>
         </div>
       </Row>
     );
