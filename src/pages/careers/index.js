73336ae55c5d9bb327bfb82681165360bc810093
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2023-04-25 09:35:40 -0400

    udpate nav on carrers and blog

--- src/pages/careers/index.js
+++ src/pages/careers/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import SubHero from "../components/SubHero/SubHero";
-import NavBar from "../components/navBar/NavBar";
+import NavBar from "../components/navBar/_NewNavBar";
 import Container from "react-bootstrap/Container";
 import ContactUs from "../components/ContactUs/ContactUs";
 import Footer from "../components/Footer/Footer";
 import Careers from "./Careers";
-import ImgCarrers from './Mask Group 2.png'
+import ImgCarrers from "./Mask Group 2.png";
 
 const CareersPage = () => {
   return (
     <>
       <NavBar />
       <Container fluid className="App m-0">
-        <SubHero title="Careers" bgImage={ImgCarrers}/>
+        <SubHero title="Careers" bgImage={ImgCarrers} />
         <Careers />
         {/* <ContactUs /> */}
         <Footer />
