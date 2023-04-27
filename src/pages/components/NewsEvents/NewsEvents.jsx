00574438dca046eb52bf55cbde89d2180d8da6db
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2023-04-27 09:07:33 -0400

    optimize imgs 3

--- src/pages/components/NewsEvents/NewsEvents.jsx
+++ src/pages/components/NewsEvents/NewsEvents.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
-import './NewsEvents.scss'
-import NewsImage from './Group87.png'
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import "./NewsEvents.scss";
+import NewsImage from "./Group87.jpeg";
 const NewsEvents = () => {
   return (
-    <Row className='container-news'>
-      <Col className='news-data col-12 col-lg-6'>
+    <Row className="container-news">
+      <Col className="news-data col-12 col-lg-6">
         <div className="news-title">
           <h2>NEWS & EVENTS</h2>
         </div>
@@ -13,11 +13,11 @@ const NewsEvents = () => {
           <p> Learn about upcoming events </p>
         </div>
       </Col>
-      <Col className='news-image col-12 col-lg-6'>
+      <Col className="news-image col-12 col-lg-6">
         <img src={NewsImage} alt="" />
       </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default NewsEvents
+export default NewsEvents;
