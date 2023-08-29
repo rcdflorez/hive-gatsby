391c94391c1a22f8b79b8ad8b8222f7aba6956a6
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2023-08-29 14:52:02 -0400

    testing

--- src/pages/crm-test.js
+++ src/pages/crm-test.js
@@ -11,16 +11,16 @@ const CrmTest = () => {
       <Container fluid className="App m-0">
         <SubHero title="CRM Test" />
         <>
-          <div data-form-block-id="b7cccc4c-483d-ee11-bdf4-00224805c558"></div>{" "}
           <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
-          <div id="dgwZJOJD-mR9iYS3quQym7qtGdyntAGqYbi09fGybvBA"></div>
-          <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/ws-tracking.js?v=1.84.2007"></script>
           <div
-            className="d365-mkt-config"
+            class="d365-mkt-config"
             style={{ display: "none" }}
-            data-website-id="gwZJOJD-mR9iYS3quQym7qtGdyntAGqYbi09fGybvBA"
+            data-website-id="c-jQq1kWu5_9Q4A3WfCcA7xN-QQ22Y0eNTvFiEUFxl4"
             data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
-          ></div>
+          >
+            {" "}
+          </div>{" "}
+          <div data-form-block-id="94c450a0-493d-ee11-bdf4-00224805c558"></div>
         </>
       </Container>
     </>
