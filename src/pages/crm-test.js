b61cd5d740b813f5abfd8223651ce0441c80ef80
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2023-09-06 10:01:30 -0400

    crm integration form

--- src/pages/crm-test.js
+++ src/pages/crm-test.js
@@ -13,7 +13,7 @@ const CrmTest = () => {
         <>
           <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
           <div
-            class="d365-mkt-config"
+            className="d365-mkt-config"
             style={{ display: "none" }}
             data-website-id="c-jQq1kWu5_9Q4A3WfCcA7xN-QQ22Y0eNTvFiEUFxl4"
             data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
