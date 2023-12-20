2614c4d0c6d8fc6309e5df2402052b5f18a2f7c5
Ricardo Flórez <59479135+rcdflorez@users.noreply.github.com>
2023-12-20 10:27:18 -0500

    Update crm-test.js

--- src/pages/crm-test.js
+++ src/pages/crm-test.js
@@ -11,16 +11,7 @@ const CrmTest = () => {
       <Container fluid className="App m-0">
         <SubHero title="CRM Test" />
         <>
-          <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
-          <div
-            className="d365-mkt-config"
-            style={{ display: "none" }}
-            data-website-id="c-jQq1kWu5_9Q4A3WfCcA7xN-QQ22Y0eNTvFiEUFxl4"
-            data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
-          >
-            {" "}
-          </div>{" "}
-          <div data-form-block-id="86bf7841-169a-ee11-be37-002248081fc5"></div>
+
 
 
          
