d012e70a522928de5e333f0bcfadb3411603da49
Ricardo Flórez <59479135+rcdflorez@users.noreply.github.com>
2023-12-20 10:16:30 -0500

    Update crm-test.js

--- src/pages/crm-test.js
+++ src/pages/crm-test.js
@@ -20,8 +20,7 @@ const CrmTest = () => {
           >
             {" "}
           </div>{" "}
-          <div data-form-block-id="86bf7841-169a-ee11-be37-002248081fc5" data-form-api-url='https://public-usa.mkt.dynamics.com/api/v1.0/orgs/30abe4d4-b070-4218-9b36-a15cd505d97b/eventmanagement'
-        data-cached-form-url='https://assets-usa.mkt.dynamics.com/30abe4d4-b070-4218-9b36-a15cd505d97b/digitalassets/forms/86bf7841-169a-ee11-be37-002248081fc5' data-readable-event-id='JTX_RT_Event_Test_1304669132'></div>
+          <div data-form-block-id="86bf7841-169a-ee11-be37-002248081fc5"></div>
         </>
       </Container>
     </>
