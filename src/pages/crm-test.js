e10934bc10144f3c1ede5318ff57d1d5ba7c0e02
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2024-01-06 18:13:36 -0500

    update form

--- src/pages/crm-test.js
+++ src/pages/crm-test.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-
-import SubHero from "./components/SubHero/SubHero";
-import NavBar from "./components/navBar/NavBar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const CrmTest = () => {
   return (
     <>
-      <NavBar />
-      <Container fluid className="App m-0">
-        <SubHero title="CRM Test" />
+      <Container fluid className="App m-0 reg-from-parent">
         <>
           {/*
           <div
@@ -20,19 +17,117 @@ const CrmTest = () => {
           ></div>
           <script src="https://cxppusa1formui01cdnsa01-endpoint.azureedge.net/usa/FormLoader/FormLoader.bundle.js"></script>
        */}
-          <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
-          <div
-            className="d365-mkt-config dynamics-form"
-            style={{ display: "none" }}
-            data-website-id="ob_8QB_B5qaOqQwmAV96PFmbbEPlpTzIn8NH_V16glI"
-            data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
-          >
-            {" "}
-          </div>{" "}
-          <div
-            className="dynamics-form-2"
-            data-form-block-id="5d55ea41-3fab-ee11-be37-00224805cb79"
-          ></div>
+          <Container className="reg-form-container">
+            <Row>
+              <Col className="text-center">
+                {" "}
+                <img
+                  src={
+                    "https://bb0f359ca8.imgdist.com/public/users/Integrators/BeeProAgency/1114299_1099789/logo_hive%204.png"
+                  }
+                  alt="hfa logo"
+                  width={400}
+                  style={{ aspectRatio: "auto 1122 / 1122" }}
+                  className="pt-5"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center top-header">
+                <h5>Q4 FY 2023</h5>
+                <h1>INVESTOR UPDATE</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center">
+                <img
+                  width={50}
+                  className="w-full img-fluid py-5 ax-center ay-middle img-icon"
+                  src={"https://hfacms.leapcredit.com/email-assets/2.png"}
+                  alt="hfa icon"
+                />
+                <div class="custom-box">
+                  <div class="header rounded-top">
+                    <h6 class="ax-center">WHAT</h6>
+                  </div>
+                  <div class="content rounded-bottom ax-center text-center">
+                    Q4 FY2023 Investor Update
+                  </div>
+                </div>
+              </Col>
+              <Col className="text-center">
+                <img
+                  width={50}
+                  className="w-full img-fluid py-5 ax-center ay-middle img-icon"
+                  src={"https://hfacms.leapcredit.com/email-assets/1.png"}
+                  alt="hfa icon"
+                />
+                <div class="custom-box">
+                  <div class="header rounded-top">
+                    <h6 class="ax-center">WHEN</h6>
+                  </div>
+                  <div class="content rounded-bottom ax-center text-center">
+                    Wednesday, February 7 4:30 PM – 6:00 PM
+                  </div>
+                </div>
+              </Col>
+            </Row>
+            <Row className="p-0">
+              <Col className="text-center">
+                <img
+                  width={50}
+                  className="w-full img-fluid py-5 ax-center ay-middle img-icon"
+                  src={
+                    "https://hfacms.leapcredit.com/email-assets/location.png"
+                  }
+                  alt="hfa icon"
+                />
+                <div class="custom-box">
+                  <div class="header rounded-top">
+                    <h6 class="ax-center">WHERE</h6>
+                  </div>
+                  <div class="content rounded-bottom ax-center text-center">
+                    3344 Peachtree Road, Suite 2600, Atlanta GA
+                  </div>
+                </div>
+              </Col>
+              <Col className="text-center">
+                <img
+                  width={50}
+                  className="w-full img-fluid py-5 ax-center ay-middle img-icon"
+                  src={"https://hfacms.leapcredit.com/email-assets/3.png"}
+                  alt="hfa icon"
+                />
+                <div class="custom-box">
+                  <div class="header rounded-top">
+                    <h6 class="ax-center">LIVE STREAM</h6>
+                  </div>
+                  <div class="content rounded-bottom ax-center text-center">
+                    Begins at 5:00 PM ET
+                  </div>
+                </div>
+              </Col>
+            </Row>
+            <Row className="text-center py-5">
+              <h1>
+                We cordially invite you to register for our Q4 FY2023 Investor
+                Update, on Wednesday, February 7.
+              </h1>
+            </Row>
+            <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
+            <div
+              className="d365-mkt-config dynamics-form"
+              style={{ display: "none" }}
+              data-website-id="ob_8QB_B5qaOqQwmAV96PFmbbEPlpTzIn8NH_V16glI"
+              data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
+            >
+              {" "}
+            </div>{" "}
+            <div
+              className="dynamics-form-2"
+              data-form-block-id="5d55ea41-3fab-ee11-be37-00224805cb79"
+            ></div>
+          </Container>
         </>
       </Container>
     </>
