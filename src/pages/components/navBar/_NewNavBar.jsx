04c8245e792584267cb4cc3107df7466ccdeefd0
Ricardo Florz <ricardo.florez@hivefs.com>
2025-08-19 13:49:22 -0400

    initial badge implementation

--- src/pages/components/navBar/_NewNavBar.jsx
+++ src/pages/components/navBar/_NewNavBar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./_NewNavBar.scss";
 import Logo from "./_newLogo.png";
 import MenuIcon from "./_menuIcon.svg";
+import TWPBadgePortrait from "../../../images/TWP_Atlanta_Portrait_2025.png";
+import TWPBadgeLandscape from "../../../images/TWP_Atlanta_2025.png";
 import { Link } from "gatsby";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -11,13 +13,22 @@ class CustNavbar extends React.Component {
   listener = null;
   state = {
     nav: false,
+    isMobile: false,
   };
   componentDidMount() {
     window.addEventListener("scroll", this.handleScroll);
+    this.checkScreenSize();
+    window.addEventListener("resize", this.checkScreenSize);
   }
   componentWillUnmount() {
     window.removeEventListener("scroll", null);
+    window.removeEventListener("resize", this.checkScreenSize);
   }
+  
+  checkScreenSize = () => {
+    this.setState({ isMobile: window.innerWidth < 768 });
+  };
+  
   handleScroll = () => {
     if (window.pageYOffset > 150) {
       if (!this.state.nav) {
@@ -41,7 +52,21 @@ class CustNavbar extends React.Component {
     return (
       <>
         <Navbar className=" Nav w-100 " bg="light" expand="">
-          <Container fluid>
+          <Container fluid style={{ position: 'relative' }}>
+            {/* Top Workplaces Badge - positioned on the left */}
+            <img
+              src={this.state.isMobile ? TWPBadgePortrait : TWPBadgeLandscape}
+              style={{
+                position: 'absolute',
+                left: '15px',
+                top: '50%',
+                transform: 'translateY(-50%)',
+                height: this.state.isMobile ? "60px" : "90px",
+                width: "auto",
+                zIndex: 10
+              }}
+              alt="Top Workplaces 2025"
+            />
             <Navbar.Brand href="#" className="mx-auto  ">
               <Link to="/">
                 <img
